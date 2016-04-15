589dcb2313175fb94fd9764a4f7882109b596b74
Adrien Antoine <adriantoine@gmail.com>
2016-04-15 13:20:09 +0100

    Added xo and reformatting

--- src/Foo.js
+++ src/Foo.js
@@ -1,20 +1,19 @@
-import React, { PropTypes } from 'react';
+import React, {PropTypes} from 'react';
 
-const propTypes = {};
-
-const defaultProps = {};
+const propTypes = {
+	children: PropTypes.any
+};
 
 class Foo extends React.Component {
-  render() {
-    return (
-      <div className="foo">
-        {this.props.children}
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div className="foo">
+				{this.props.children}
+			</div>
+		);
+	}
 }
 
 Foo.propTypes = propTypes;
-Foo.defaultProps = defaultProps;
 
 export default Foo;
--- test/Foo-test.js
+++ test/Foo-test.js
@@ -8,13 +8,13 @@ import Foo from '../src/Foo';
 const expect = unexpected.clone().use(unexpectedReact);
 
 test("sometimes it succeeds", () => {
-  var renderer = TestUtils.createRenderer();
-  renderer.render(<Foo/>);
-  expect(renderer, 'to have rendered', <div className="foo"/>);
+	var renderer = TestUtils.createRenderer();
+	renderer.render(<Foo/>);
+	expect(renderer, 'to have rendered', <div className="foo"/>);
 });
 
 test("sometimes it fails", () => {
-  var renderer = TestUtils.createRenderer();
-  renderer.render(<Foo>Bar</Foo>);
-  expect(renderer, 'to have rendered', <div className="foo2">Bar2</div>);
+	var renderer = TestUtils.createRenderer();
+	renderer.render(<Foo>Bar</Foo>);
+	expect(renderer, 'to have rendered', <div className="foo2">Bar2</div>);
 });
